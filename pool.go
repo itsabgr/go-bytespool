fd79630b9c2e43f1222ba9d60dc12b1c2b9dfe08
abgr <tbpmtbpm@gmail.com>
2021-08-17 02:23:55 +0430

    add docs

--- pool.go
+++ pool.go
@@ -6,17 +6,22 @@ import (
 
 var bytes Q
 
+//MinBytesLen is minimum bytes len to add to pool
 var MinBytesLen = 16
+
+//Put adds b to pull
 func Put(b []byte) {
-	if len(b) <= MinBytesLen{
+	if len(b) <= MinBytesLen {
 		return
 	}
 	bytes.Push(b)
 }
-func Get(max int)  []byte{
-	IByte,found := bytes.Pull()
-	if !found{
-		return make([]byte,max)
+
+//Get return bytes[:max]
+func Get(max int) []byte {
+	IByte, found := bytes.Pull()
+	if !found {
+		return make([]byte, max)
 	}
 	b := IByte.([]byte)
 	if len(b) > max {
